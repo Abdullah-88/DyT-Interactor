e0617fee7f01bb29e5eadcc5b4ae4a8dbae01f91
Abdullah Nazhat Abdullah <abdullah.nazhat@gmail.com>
2026-08-24 00:31:28 +0300

    Update contextdyt.py

--- contextdyt.py
+++ contextdyt.py
@@ -19,21 +19,21 @@ class GlobalDynamicTanh(nn.Module):
         super().__init__()
         self.normalized_shape = normalized_shape
         self.alpha_init_value = alpha_init_value      
-        self.alpha = nn.Parameter(torch.ones(normalized_shape*sequence_length) * alpha_init_value)
+        self.alpha = nn.Parameter(torch.ones(normalized_shape * sequence_length) * alpha_init_value)
        
     def forward(self, x):
         x = torch.tanh(self.alpha * x)
         return x 
 
 class MappingUnit(nn.Module):
-    def __init__(self,dim):
+    def __init__(self,dim, hidden_dim):
         super().__init__()
                    
         self.dyt_token = DynamicTanh(dim)
         self.gelu = nn.GELU()
-        self.proj_1 =  nn.Linear(dim,dim,bias = False)
-        self.proj_2 =  nn.Linear(dim,dim,bias = False)
-        self.proj_3 = nn.Linear(dim,dim,bias = False)
+        self.proj_1 =  nn.Linear(dim, hidden_dim, bias = False)
+        self.proj_2 =  nn.Linear(dim, hidden_dim, bias = False)
+        self.proj_3 = nn.Linear(hidden_dim, dim, bias = False)
              	   
     def forward(self, x):
     
@@ -48,11 +48,11 @@ class MappingUnit(nn.Module):
     	return x
     	
 class InteractionUnit(nn.Module):
-    def __init__(self,dim,num_tokens):
+    def __init__(self, dim, num_tokens):
         super().__init__()
                      
         self.dyt_token = DynamicTanh(dim) 
-        self.dyt_context = GlobalDynamicTanh(dim,num_tokens)    
+        self.dyt_context = GlobalDynamicTanh(dim, num_tokens)    
                     	   
     def forward(self, x):
     
@@ -69,11 +69,11 @@ class InteractionUnit(nn.Module):
     	return x
     	
 class ContextDyTBlock(nn.Module):
-    def __init__(self, d_model, num_tokens):
+    def __init__(self, d_model, d_ffn, num_tokens):
         super().__init__()
                 
-        self.mapping = MappingUnit(d_model)
-        self.interaction = InteractionUnit(d_model,num_tokens)
+        self.mapping = MappingUnit(d_model, d_ffn)
+        self.interaction = InteractionUnit(d_model, num_tokens)
         
     def forward(self, x):
                   
@@ -92,11 +92,11 @@ class ContextDyTBlock(nn.Module):
         return out
 
 class ContextDyT(nn.Module):
-    def __init__(self, d_model, num_tokens, num_layers):
+    def __init__(self, d_model, d_ffn, num_tokens, num_layers):
         super().__init__()
         
         self.model = nn.Sequential(
-            *[ContextDyTBlock(d_model, num_tokens) for _ in range(num_layers)]
+            *[ContextDyTBlock(d_model, d_ffn, num_tokens) for _ in range(num_layers)]
         )
 
     def forward(self, x):
